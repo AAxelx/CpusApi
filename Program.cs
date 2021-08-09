90ecce2f7aa92e215fa1d3b6955e4c956835fef1
DESKTOP-JCH77E3\Ananasik <kravchenko.s1111@gmail.com>
2021-08-09 16:34:01 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CpusApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
